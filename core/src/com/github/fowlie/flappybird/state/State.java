b9f5ff5072df8aa27841fcb1a3a2283af5f58982
Mats Faugli <mats.faugli@if.no>
2015-08-04 00:00:06 +0200

    Attempt to publish the javascript version

--- core/src/com/github/fowlie/flappybird/state/State.java
+++
@@ -1,9 +0,0 @@
-package com.github.fowlie.flappybird.state;
-
-import com.github.fowlie.flappybird.GUI;
-import com.github.fowlie.flappybird.World;
-
-public interface State {
-    State handleInput(World world);
-    State update(World world, GUI gui);
-}
